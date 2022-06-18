1c005701561d2df6a1a54a6742e17034f6c3890f
ViktorIshchenko <54843443+ViktorIshchenko@users.noreply.github.com>
2022-06-18 11:23:14 +0300

    Update jsonfilter.py
    def sendJsonToRmq(msg):
    
        slack = os.environ.get('QUEUE_SLACK')
        restapi = os.environ.get('QUEUE_RESTAPI')
        
        MQ_CONNECTION = os.environ.get("MQ_CONNECTION")
    
        if MQ_CONNECTION == 'SSL':
            RABBIT_HOST = os.environ.get("RABBIT_HOST")
            RABBIT_PORT = os.environ.get("RABBIT_PORT")
            RABBIT_USER = os.environ.get("RABBIT_USER")
            RABBIT_PW = os.environ.get("RABBIT_PW")
            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
            ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
            parameters = pika.URLParameters(f"amqp://{RABBIT_USER}:{RABBIT_PW}@{RABBIT_HOST}:{RABBIT_PORT}")
            parameters.ssl_options = pika.SSLOptions(context=ssl_context)
            connection = pika.BlockingConnection(parameters)  
        else:
            connection = pika.BlockingConnection(
            pika.ConnectionParameters(host=os.environ.get('RMQ_HOST'), port=os.environ.get('RMQ_PORT'), credentials=pika.PlainCredentials(os.environ.get('RMQ_LOGIN'), os.environ.get('RMQ_PASS'))))
            channel = connection.channel()
            channel.queue_declare(queue=slack)
            channel.queue_declare(queue=restapi)
    
        channel.basic_publish(exchange='', routing_key=os.environ.get('QUEUE_SLACK'), body=msg)
        channel.basic_publish(exchange='', routing_key=os.environ.get('QUEUE_RESTAPI'), body=msg)
    
        print("Done!")
        connection.close()

--- jsonfilter.py
+++ jsonfilter.py
@@ -23,19 +23,19 @@ def sendJsonToRmq(msg):
         RABBIT_USER = os.environ.get("RABBIT_USER")
         RABBIT_PW = os.environ.get("RABBIT_PW")
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')    
+        ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
         parameters = pika.URLParameters(f"amqp://{RABBIT_USER}:{RABBIT_PW}@{RABBIT_HOST}:{RABBIT_PORT}")
+        parameters.ssl_options = pika.SSLOptions(context=ssl_context)
+        connection = pika.BlockingConnection(parameters)  
     else:
         connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=os.environ.get('RMQ_HOST'), port=os.environ.get('RMQ_PORT'), credentials=pika.PlainCredentials(os.environ.get('RMQ_LOGIN'), os.environ.get('RMQ_PASS'))))
-        channel = connection.channel()       
-        parameters.ssl_options = pika.SSLOptions(context=ssl_context)
-        connection = pika.BlockingConnection(parameters)       
         channel = connection.channel()
         channel.queue_declare(queue=slack)
-        channel.queue_declare(queue=restapi)        
-        channel.basic_publish(exchange='', routing_key=os.environ.get('QUEUE_SLACK'), body=msg)
-        channel.basic_publish(exchange='', routing_key=os.environ.get('QUEUE_RESTAPI'), body=msg)
+        channel.queue_declare(queue=restapi)
+
+    channel.basic_publish(exchange='', routing_key=os.environ.get('QUEUE_SLACK'), body=msg)
+    channel.basic_publish(exchange='', routing_key=os.environ.get('QUEUE_RESTAPI'), body=msg)
 
     print("Done!")
     connection.close()
